ed21bfac7e78055948b35ab6aba5cd039976599f
Krishna-118 <krishnakumar127127@gmail.com>
2026-02-03 17:45:19 +0530

    utils files are added to process the pdfs

---
+++ utils.py
@@ -0,0 +1,32 @@
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
+import os
+
+def process_pdfs(pdf_files):
+    documents = []
+
+    for pdf in pdf_files:
+        with open(pdf.name, "wb") as f:
+            f.write(pdf.getbuffer())
+
+        loader = PyPDFLoader(pdf.name)
+        docs = loader.load()
+        documents.extend(docs)
+
+        os.remove(pdf.name)
+
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=1000,
+        chunk_overlap=150
+    )
+
+    chunks = text_splitter.split_documents(documents)
+
+    embeddings = HuggingFaceEmbeddings()
+    vectorstore = FAISS.from_documents(chunks, embeddings)
+    
+    print(documents)
+
+    return vectorstore
